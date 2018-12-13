34929ed056362fcf6ed07575dd78fd34bb6922e1
Kevin Hermawan <kevinhermawanx@gmail.com>
2018-12-13 14:16:06 +0700

    Updates

---
+++ precache-manifest.633ef68a862934ce8a101db52bbfc297.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "b5afb11af8e07725bae4",
+    "url": "/rnw-news/static/js/runtime~main.b5afb11a.js"
+  },
+  {
+    "revision": "73802e207e026261f8e2",
+    "url": "/rnw-news/static/js/main.73802e20.chunk.js"
+  },
+  {
+    "revision": "1148d33bbe74e9fc98cc",
+    "url": "/rnw-news/static/js/1.1148d33b.chunk.js"
+  },
+  {
+    "revision": "5bac1a0c4df5ac9d59380d8527abcf44",
+    "url": "/rnw-news/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.48e0ac30c84a3addcf5605255a471af0.js"
+  "/rnw-news/precache-manifest.633ef68a862934ce8a101db52bbfc297.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/rnw-news/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
